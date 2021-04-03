b8c5092fd25f90b541476035f9c0ef1228099c84
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-04-03 14:32:34 -0300

    Criando component para a entidade Pages.

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 Fonte: [Laravel 8 - Building a CMS](https://www.youtube.com/playlist?list=PLSP81gW0XjNHk2D2NREM8A80xWO19Yulj)
 
+Repositório oficial: [github.com/jackoftraits/laravel8-with-livewire](https://github.com/jackoftraits/laravel8-with-livewire)
+
 * [Aula 1 - Laravel 8 Livewire Tutorial (Building a Simple CMS) : Installation (part1)](https://www.youtube.com/watch?v=Ub6FMEWw7kA&list=PLSP81gW0XjNHk2D2NREM8A80xWO19Yulj&index=1)
 * [Aula 2 - Laravel 8 Livewire Tutorial (Building a Simple CMS) : Create Operation using Jetstream Modal (part2)](https://www.youtube.com/watch?v=xX1qmJwGqg4&list=PLSP81gW0XjNHk2D2NREM8A80xWO19Yulj&index=2)
+
+https://youtu.be/xX1qmJwGqg4?t=773
--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,103 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 
 class Pages extends Component
 {
+    public $modalFormVisible = true;
+    public $slug;
+    public $title;
+    public $content;
+
+    /**
+     * The validation rules.
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages', 'slug')],
+            'content' => 'required'
+        ];
+    }
+
+    /**
+     * Run everytime the title
+     * variable is updated.
+     *
+     * @param $value
+     */
+    public function updatedTitle($value)
+    {
+        $this->generateSlug($value);
+    }
+
+    /**
+     * The create function.
+     */
+    public function create()
+    {
+        $this->validate();
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+        $this->resetVars();
+    }
+
+    /**
+     * Show the form modal
+     * of the create function.
+     */
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
+    /**
+     * The data for the model mapped
+     * in this component.
+     *
+     * @return array
+     */
+    public function  modelData()
+    {
+        return [
+          'title' => $this->title,
+          'slug' => $this->slug,
+          'content' => $this->content
+        ];
+    }
+
+    /**
+     * Resets all the variables
+     * to null.
+     */
+    public function resetVars()
+    {
+        $this->title = null;
+        $this->slug = null;
+        $this->content = null;
+    }
+
+    /**
+     * Generate a url slug
+     * base on the title
+     * @param $value
+     */
+    private function generateSlug($value)
+    {
+        $process1 = str_replace(' ', '-', $value);
+        $process2 = strtolower($process1);
+        $this->slug = $process2;
+    }
+
+    /**
+     * The Livewire render function.
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function render()
     {
         return view('livewire.pages');
